4e0750460c00f8a5265b9327fd7f41136560fb26
pengshuai@eastmoney.com <acheng6@126.com>
2026-07-10 13:03:33 +0800

    ems

--- spider_ems/环境部署.bat
+++ spider_ems/环境部署.bat
@@ -4,6 +4,6 @@ cd /d "%~dp0"
 echo "安装python库"
 d:\_code\python\python.exe -m pip install -r d:/_code/spider_ems/requirements.txt -i https://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 
-echo "安装playwright"
-d:\_code\python\python.exe -m playwright install
-pause
+@REM echo "安装playwright"
+@REM d:\_code\python\python.exe -m playwright install
+@REM pause
