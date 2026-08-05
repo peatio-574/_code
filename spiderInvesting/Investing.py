2eea457682142431dc3e4ec768c735cfc364b408
pengshuai@eastmoney.com <acheng6@126.com>
2026-08-05 16:12:41 +0800

    spiderInvesting

--- spiderInvesting/Investing.py
+++ spiderInvesting/Investing.py
@@ -9,7 +9,7 @@ else:
 
 sys.path.insert(0, os.path.join(BASE_DIR, '..', ))
 
-from PlayWright import Playwright_, logger
+from newPlayWright import PlayWright, logger
 import time
 import csv
 import requests
@@ -32,33 +32,33 @@ def getPageInfo(startDate):
     """循环滚动，获取符合条件的标题、链接"""
     url = 'https://cn.investing.com/search/?q=%E7%87%83%E6%96%99%E6%B2%B9&tab=news'
 
-    Playwright_.goto(url)
+    PlayWright.goto(url)
     time.sleep(5)
-    Playwright_.mouse_wheel(200)
-    Playwright_.click('(//span[@class="datePickerIcon"])[1]')
+    PlayWright.mouse_wheel(200)
+    PlayWright.click('(//span[@class="datePickerIcon"])[1]')
 
     endDate = time.strftime('%Y-%m-%d', time.localtime(time.mktime(time.strptime(startDate, "%Y-%m-%d")) + 86400))
     startStr = startDate.replace('-', '/')
     endStr = endDate.replace('-', '/')
-    Playwright_.slowInput('//input[@id="startDate"]', startStr)
+    PlayWright.slow_input('//input[@id="startDate"]', startStr)
     time.sleep(1)
-    Playwright_.slowInput('//input[@id="endDate"]', endStr, enter=True)
+    PlayWright.slow_input('//input[@id="endDate"]', endStr, enter=True)
     time.sleep(5)
 
     rowEle = '//div[@class="js-section-content largeTitle"]/div'
-    rowCount = Playwright_.get_count(rowEle)
+    rowCount = PlayWright.get_count(rowEle)
 
     rowsInfo = []
 
     for rowId in range(1, rowCount + 1):
-        date = Playwright_.get_text(f'{rowEle}[{rowId}]//time')
+        date = PlayWright.get_text(f'{rowEle}[{rowId}]//time')
         if not matchDate(date, startDate):
             continue
 
         titleEle = f'{rowEle}[{rowId}]//a[@class="title"]'
-        title = Playwright_.get_text(titleEle)
-        href = Playwright_.get_attribute(titleEle, 'href')
-        # content = Playwright_.get_text(f'{rowEle}[{rowId}]//p[@class="js-news-item-content"]')
+        title = PlayWright.get_text(titleEle)
+        href = PlayWright.get_attribute(titleEle, 'href')
+        # content = PlayWright.get_text(f'{rowEle}[{rowId}]//p[@class="js-news-item-content"]')
         rowInfo = {
             'date': date,
             'title': title,
@@ -75,12 +75,12 @@ def getRowDetail(rowInfo, startDate):
     base = 'https://cn.investing.com'
     link = base + rowInfo['href']
     for roll in range(1, 4):
-        status = Playwright_.goto(link)
+        status = PlayWright.goto(link)
         if status:
             break
     time.sleep(5)
 
-    publishTime = Playwright_.get_text('(//div[@class="flex flex-row items-center"])[2]/span')
+    publishTime = PlayWright.get_text('(//div[@class="flex flex-row items-center"])[2]/span')
     year, month, day = startDate.split('-')
     month = month if not month.startswith('0') else month[1]
     day = day if not day.startswith('0') else day[1]
@@ -89,24 +89,25 @@ def getRowDetail(rowInfo, startDate):
         return rowInfo
 
     contentEle = '//div[contains(@class, "article")]/div[1]/p'
-    contentCount = Playwright_.get_count(contentEle)
+    contentCount = PlayWright.get_count(contentEle)
     contents = []
     for contentId in range(1, contentCount + 1):
-        content = Playwright_.get_text(f'({contentEle})[{contentId}]')
+        content = PlayWright.get_text(f'({contentEle})[{contentId}]')
         contents.append(content)
     contents = '\n'.join(contents)
 
     authorEle= '//span[@class="flex flex-row text-xs"]/a/span'
-    author = Playwright_.get_text(authorEle) if Playwright_.get_count(authorEle) else ''
+    author = PlayWright.get_text(authorEle) if PlayWright.get_count(authorEle) else ''
     rowInfo['contents'] = contents
     rowInfo['publishTime'] = publishTime[5:]
     rowInfo['author'] = author
+    rowInfo['url'] = link
     return rowInfo
 
 
 def getRowsDetail(startDate):
-    csvFile = os.path.join(BASE_DIR, 'Investing.csv')
-    fileHeader = ['发布时间', '标题', '作者', '正文']
+    csvFile = os.path.join(BASE_DIR, 'yesterday.csv')
+    fileHeader = ['title', 'date', 'url', 'author', 'content']
     fileExists = os.path.exists(csvFile)
     rowsInfo = getPageInfo(startDate)
     logger.info(f'共{len(rowsInfo)}条数据')
@@ -124,14 +125,13 @@ def getRowsDetail(startDate):
 
             # 只处理有发布时间的数据
             if rowInfo.get('publishTime'):
-
-
                 # 构造符合表头格式的数据字典
                 csv_row = {
-                    '发布时间': rowInfo.get('publishTime', ''),
-                    '标题': rowInfo.get('title', ''),
-                    '作者': rowInfo.get('author', ''),
-                    '正文': rowInfo.get('contents', '')
+                    'title': rowInfo.get('title', ''),
+                    'date': rowInfo.get('publishTime', ''),
+                    'url': rowInfo.get('url', ''),
+                    'author': rowInfo.get('author', ''),
+                    'content': rowInfo.get('contents', '')
                 }
 
                 # 逐行追加写入CSV
@@ -140,6 +140,7 @@ def getRowsDetail(startDate):
 
                 # 可选：立即刷新到磁盘，防止数据丢失
                 f.flush()
+
 def download(url, file, vpn=None):
     file = file.replace('/', '-').replace(' ', '-')
     for roll in range(1, 4):
@@ -174,7 +175,7 @@ def getOpecReport(vpn):
 
 
 def getEivlist(vpn):
-    Playwright_.goto('https://www.eia.gov/reports/#/T198,T1255', proxy={'server': f'http://{vpn}'})
+    PlayWright.goto('https://www.eia.gov/reports/#/T198,T1255', proxy={'server': f'http://{vpn}'})
     time.sleep(10)
     t = time.localtime(time.time() - 86400*30)
     lastMonth = t.tm_mon
@@ -183,40 +184,55 @@ def getEivlist(vpn):
     lastMonthEn = time.strftime("%B", time.localtime(time.mktime((lastYear, lastMonth, 1, 0, 0, 0, 0, 0, 0)))).lower()
 
     rowEle = '//div[@class="b_content"]'
-    rowsCount = Playwright_.get_count(rowEle)
+    rowsCount = PlayWright.get_count(rowEle)
     data = []
     for rowIdx in range(1, rowsCount + 1):
-        publishTime = Playwright_.get_text(f'({rowEle})[{rowIdx}]/h4[@class="dat bookshelf"]')
+        publishTime = PlayWright.get_text(f'({rowEle})[{rowIdx}]/h4[@class="dat bookshelf"]')
         if lastMonthEn in publishTime.lower() and str(lastYear) in publishTime:
             pdfEle = f'({rowEle})[{rowIdx}]//a[@class="ico pdf"]'
             htmlEle = f'({rowEle})[{rowIdx}]//a[@class="ico html"]'
 
-            title = publishTime + Playwright_.get_text(f'({rowEle})[{rowIdx}]/h3/a')
+            title = publishTime + PlayWright.get_text(f'({rowEle})[{rowIdx}]/h3/a')
             extraEle = f'({rowEle})[{rowIdx}]/h3/em'
-            if Playwright_.get_count(extraEle):
-                title += Playwright_.get_text(extraEle)
+            if PlayWright.get_count(extraEle):
+                title += PlayWright.get_text(extraEle)
 
-            if Playwright_.get_count(pdfEle):
+            if PlayWright.get_count(pdfEle):
                 file = title + '.pdf'
-                url = 'https://www.eia.gov' + Playwright_.get_attribute(pdfEle, 'href')
+                url = 'https://www.eia.gov' + PlayWright.get_attribute(pdfEle, 'href')
                 logger.info(f'{title}存在pdf数据')
                 download(url, file, vpn)
                 data.append(url)
-            elif Playwright_.get_count(htmlEle):
+            elif PlayWright.get_count(htmlEle):
                 # file = title + '.html'
-                url = 'https://www.eia.gov' + Playwright_.get_attribute(htmlEle, 'href')
+                url = 'https://www.eia.gov' + PlayWright.get_attribute(htmlEle, 'href')
                 logger.info(f'{title}存在html数据，请手动复制链接\n{url}\n')
                 # download(url, file, vpn)
                 data.append(url)
     if not data:
         logger.info(f'{lastYear} {lastMonth}暂无匹配数据')
 
+def getJianXinDaily(startDate):
+    PlayWright.goto('https://www.ccbfutures.com/main/research/research_report/survey_studies/chemical_industry/index.shtml')
+    time.sleep(15)
+    rowEle = '//div[@class="imp_news clearfix"]/ul/li'
+    rowsCount = PlayWright.get_count(rowEle)
+    for rowIdx in range(1, rowsCount + 1):
+        publishTime = PlayWright.get_text(f'({rowEle})[{rowIdx}]/span[@class="time"]')
+        titleEle = f'({rowEle})[{rowIdx}]/a'
+        title = f'({publishTime})' + PlayWright.get_text(titleEle)
+        if '原油' not in title or publishTime not in startDate:
+            continue
+        href = 'https://www.ccbfutures.com' + PlayWright.get_attribute(titleEle, 'href')
+        download(href, title)
+    # PlayWright.click(f'//a[@class="ddd" and text()="{page}"]')
+
 
 
 
 if __name__ == '__main__':
     while True:
-        step = input('请输入操作步骤(1获取投资信息，2获取OPEC月度报告，3获取eiv月度报告)：')
+        step = input('请输入操作步骤(1获取投资信息，2获取OPEC月度报告，3获取eiv月度报告，4获取建信日评)：')
         if step == '1':
             startDate = input('请输入指定日期(例如：2026-07-17)：')
             getRowsDetail(startDate)
@@ -226,5 +242,12 @@ if __name__ == '__main__':
         elif step == '3':
             vpn = input('请输入VPN代理(例如：127.0.0.1:7892)：')
             getEivlist(vpn)
+        elif step == '4':
+            # startDate = input('请输入指定日期(例如：2026-07-17)：')
+            startDate = time.strftime("%Y-%m-%d", time.localtime(time.time() - 86400))
+            getJianXinDaily(startDate)
+
+            
+            
 
 
